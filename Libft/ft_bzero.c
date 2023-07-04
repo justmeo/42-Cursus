f56a08679ba9d55df4eabad7b286f81390b7b878
Yassine <mrabeti.yassine@gmail.com>
2023-07-04 14:58:24 +0400

    done

--- Libft/ft_bzero.c
+++ Libft/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: ymrabeti <ymrabeti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 14:35:03 by ymrabeti          #+#    #+#             */
-/*   Updated: 2023/07/04 14:45:46 by ymrabeti         ###   ########.fr       */
+/*   Updated: 2023/07/04 14:57:03 by ymrabeti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,19 @@ void	ft_bzero(void *s, size_t n)
 		v[i] = 0;
 		i++;
 	}
-	s = v;
-	return (v);
 }
+
+// #include <stdio.h>
+// #include <string.h>
+  
+// int main()
+// {
+//     char str[50] = "yassine";
+//     printf("\nBefore memset(): %s\n", str);
+  
+//     // Fill 8 characters starting from str[13] with '.'
+//     ft_bzero(str + 3, 8);
+  
+//     printf("After memset():  %s", str);
+//     return 0;
+// }
