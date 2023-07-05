2fe4be014465d4eb3afe6b6360102ae65d53b999
Yassine <mrabeti.yassine@gmail.com>
2023-07-05 13:22:26 +0400

    done

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -6,11 +6,12 @@
 /*   By: ymrabeti <ymrabeti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 10:20:54 by ymrabeti          #+#    #+#             */
-/*   Updated: 2023/07/05 10:46:06 by ymrabeti         ###   ########.fr       */
+/*   Updated: 2023/07/05 13:22:18 by ymrabeti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include <stdio.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
@@ -23,3 +24,11 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 		*d++ = *s++;
 	return (dest);
 }
+
+// int	main(void)
+// {
+//     char str1[20];
+//     char str2[] = "yassine";
+// 	printf("%s\n", ft_memmove(str1 + 3, str2, 5));
+// 	printf("%s", memmove(str1 + 3, str2, 5));
+// }
