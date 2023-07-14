155db8fffb70a0127213832f07601d36d359ddc0
Yassine <mrabeti.yassine@gmail.com>
2023-07-14 16:14:45 +0400

    done

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -1,31 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ymrabeti <ymrabeti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/11 10:50:40 by ymrabeti          #+#    #+#             */
-/*   Updated: 2023/07/14 13:57:19 by ymrabeti         ###   ########.fr       */
+/*   Created: 2023/07/14 16:01:17 by ymrabeti          #+#    #+#             */
+/*   Updated: 2023/07/14 16:11:58 by ymrabeti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t count, size_t size)
+char *ft_strjoin(char const *s1, char const *s2)
 {
-	size_t u;
-	size_t i;
-
-	i = 0;
-	u = count * size;
-	char *me = (char *)malloc(u);
-	if (!me)
-		return (NULL);
-	while (i < u)
-	{
-		me[i] = 0;
-		i++;
-	}
-	return (me);
+    
 }
--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,14 +6,49 @@
 /*   By: ymrabeti <ymrabeti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 10:41:18 by ymrabeti          #+#    #+#             */
-/*   Updated: 2023/07/13 15:34:17 by ymrabeti         ###   ########.fr       */
+/*   Updated: 2023/07/14 14:21:41 by ymrabeti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_substr(char const *s,unsigned int start,size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-    char *me = (char *)malloc(len * sizeof(char));
-    
+	char	*me;
+	size_t	i;
+
+	if (start >= ft_strlen(s))
+	{
+		me = (char *)malloc((1) * sizeof(char));
+		me[0] = '\0';
+		return (me);
+	}
+	me = (char *)malloc((len + 1) * sizeof(char));
+	i = 0;
+	if (!me)
+		return (NULL);
+	while (i < len && s[start] != '\0')
+	{
+		me[i] = s[start];
+		start++;
+		i++;
+	}
+	me[i] = '\0';
+	return (me);
 }
+
+// int	main(void)
+// {
+// 	const char	*original;
+// 	char		*substring;
+
+// 	original = "Hello, world!";
+// 	substring = ft_substr(original, 7, 5);
+// 	if (substring != NULL)
+// 	{
+// 		printf("Original string: %s\n", original);
+// 		printf("Substring: %s\n", substring);
+// 		free(substring);
+// 	}
+// 	return (0);
+// }
