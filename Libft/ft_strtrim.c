e6a217091334f29b5e85baf83bd9d3219a79fb40
Yassine <mrabeti.yassine@gmail.com>
2023-07-16 07:54:47 +0400

    done

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -1,41 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ymrabeti <ymrabeti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/14 16:01:17 by ymrabeti          #+#    #+#             */
-/*   Updated: 2023/07/14 16:30:05 by ymrabeti         ###   ########.fr       */
+/*   Created: 2023/07/14 16:52:01 by ymrabeti          #+#    #+#             */
+/*   Updated: 2023/07/14 16:57:13 by ymrabeti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strtrim(char const *s1, char const *set)
+
 {
 	char *me;
 	size_t i;
 	size_t j;
+	size_t k;
 
-	if (!s1 || !s2)
-		return (NULL);
-	me = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
-	if (!me)
+	if (!s1 || !set)
 		return (NULL);
 	i = 0;
-	j = 0;
-	while (s1[i] != '\0')
-	{
-		me[i] = s1[i];
+	j = ft_strlen(s1) - 1;
+	k = 0;
+	while (s1[i] && ft_strchr(set, s1[i]))
 		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		me[i] = s2[j];
-		i++;
-		j++;
-	}
-	me[i] = '\0';
+	while (j > i && ft_strchr(set, s1[j]))
+		j--;
+	me = (char *)malloc((j - i + 2) * sizeof(char));
+	if (!me)
+		return (NULL);
+	while (i <= j)
+		me[k++] = s1[i++];
+	me[k] = '\0';
 	return (me);
 }
