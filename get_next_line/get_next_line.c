dccb6b64e613d347a159fc9560940bf8a2e86c73
Yassine <mrabeti.yassine@gmail.com>
2023-08-09 14:06:45 +0400

    done

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,53 +6,41 @@
 /*   By: ymrabeti <ymrabeti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 09:14:27 by ymrabeti          #+#    #+#             */
-/*   Updated: 2023/08/04 17:01:30 by ymrabeti         ###   ########.fr       */
+/*   Updated: 2023/08/09 13:57:18 by ymrabeti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-// static char	*put(int fd)
-// {
-// 	char	*split;
-// 	char	*me;
-// 	size_t	bytesRead;
-// 	int i;
-
-// 	i=0;
-// 	split = (char *)malloc(BUFFER_SIZE * sizeof(char));
-// 	if (!split)
-// 		return (NULL);
-// 	me = (char *)malloc(BUFFER_SIZE * sizeof(char));
-// 	if (!me)
-// 		return (NULL);
-// 	bytesRead = read(fd, split, BUFFER_SIZE);
-// 	if (bytesRead == -1)
-// 	{
-// 		free(split);
-// 		return (NULL);
-// 	}
-// 	me = ft_strjoin(me,split);
-// 	while (me[i] != '\n' && me[i] != '\0')
-// 	{
-// 		i++;
-// 	}
-// 	i++;
-
-// 	return (me);
-// }
-
 char	*rm_str(char *me)
 {
 	while (*me != '\n')
 	{
-		me++;
 		if (*me == '\0')
+		{
+			// free(me);
+			// me = malloc(1 * sizeof(char));
+			// me = NULL;
 			return (NULL);
+		}
+		me++;
 	}
 	if (*me == '\n')
 		me++;
 	return (me);
+}
+char	*tired(char *me)
+{
+	char	*you;
+
+	if (rm_str(me) != NULL)
+	{
+		you = ft_strdup(rm_str(me));
+		free(me);
+	}
+	else
+		return (rm_str(me));
+	return (you);
 }
 
 char	*get_next_line(int fd)
@@ -68,28 +56,91 @@ char	*get_next_line(int fd)
 	{
 		buffer[bytes] = '\0';
 		if (!me)
-			me = strdup(buffer);
+			me = ft_strdup(buffer);
 		else
 			me = ft_strjoin(me, buffer);
 		if (ft_strchr(me, '\n'))
 		{
-			line = strdup(me);
-			line = opp_strchr(line, '\n');
-			me = rm_str(me);
+			line = opp_strchr(me, '\n');
+			me = tired(me);
 			return (line);
 		}
-		bytes = 0;
 	}
 	if (bytes == 0 && me)
 	{
-		line = strdup(me);
-		line = opp_strchr(line, '\n');
-		me = rm_str(me);
-		// me = NULL;
+		line = opp_strchr(me, '\n');
+		me = tired(me);
 		return (line);
 	}
 	return (NULL);
 }
+
+// char	*get_next_line(int fd)
+// {
+// 	static char	*me;
+// 	char		*you;
+// 	char		*them;
+// 	char		buffer[BUFFER_SIZE + 1];
+// 	int			i;
+
+// 	if (fd < 0)
+// 		return (NULL);
+// 	if (!me)
+// 		me = malloc(BUFFER_SIZE + 1 * sizeof(char));
+// 	while ((i = read(fd, buffer, BUFFER_SIZE)) > 0)
+// 	{
+// 		buffer[i] = '\0';
+// 		if (ft_strchr(me, '\n'))
+// 		{
+// 			them = opp_strchr(me, '\n');
+// 			me = rm_str(me);
+// 			you = malloc((ft_strlen(me) + 1) * sizeof(char));
+// 			ft_memcpy(you, me, ft_strlen(you));
+// 			free(me);
+// 			me = malloc(ft_strlen(you) * sizeof(char));
+// 			ft_memcpy(me, you, ft_strlen(you));
+// 			return (them);
+// 		}
+// 		me = ft_strjoin(me, buffer);
+// 	}
+// 	if (i == 0 && me)
+// 	{
+// 		them = opp_strchr(me, '\n');
+// 		you = malloc(ft_strlen(rm_str(me)) + 1 * sizeof(char));
+// 		you = rm_str(me);
+// 		free(me);
+// 		ft_memcpy(me, you, ft_strlen(you));
+// 		return (them);
+// 	}
+// 	return (NULL);
+// }
+
+// char	*get_next_line(int fd)
+// {
+// 	int		line_size;
+// 	char	c;
+// 	char	*line;
+
+// 	if (fd < 0)
+// 		return (NULL);
+// 	line = NULL;
+// 	line_size = 0;
+// 	while (read(fd, &c, 1) > 0)
+// 	{
+// 		if (line_size % BUFFER_SIZE == 0)
+// 			line = realloc(line, (line_size + BUFFER_SIZE) * sizeof(char));
+// 		line[line_size++] = c;
+// 		if (c == '\n')
+// 			break ;
+// 	}
+// 	if (line_size == 0)
+// 	{
+// 		free(line);
+// 		return (NULL);
+// 	}
+// 	line[line_size] = '\0';
+// 	return (line);
+// }
 
 // int	main(void)
 // {
@@ -103,22 +154,138 @@ char	*get_next_line(int fd)
 // 		perror("Error opening the file");
 // 		return (1);
 // 	}
-// 	printf("%s$", get_next_line(fd));
-// 	printf("%s$", get_next_line(fd));
-// 	printf("%s$", get_next_line(fd));
-// 	printf("%s$", get_next_line(fd));
+// 	// printf("%s$", get_next_line(fd));
+// 	// printf("%s$", get_next_line(fd));
+// 	// printf("%s$", get_next_line(fd));
+// 	// printf("%s$", get_next_line(fd));
 
-// 	// line = get_next_line(fd);
-// 	// // printf("%s\n", get_next_line(fd));
+// 	line = get_next_line(fd);
+// 	// printf("%s\n", get_next_line(fd));
 
-// 	// while (line != NULL)
-// 	// {
-// 	// 	printf("%s", line);
-// 	// 	free(line);
-// 	// 	line = get_next_line(fd);
-// 	// }
+// 	while (line != NULL)
+// 	{
+// 		printf("%s", line);
+// 		free(line);
+// 		line = get_next_line(fd);
+// 	}
 
 // 	close(fd);
 
 // 	return (0);
+// }
+
+// char	*get_next_line(int fd)
+// {
+// 	static char *me;
+// 	char *you;
+// 	char *line;
+// 	char buffer[BUFFER_SIZE + 1];
+// 	int bytes;
+// 	size_t i;
+// 	size_t j;
+// 	size_t k;
+// 	size_t a;
+// 	size_t b;
+
+// 	if (fd < 0 || BUFFER_SIZE <= 0)
+// 		return (NULL);
+
+// 	while ((bytes = read(fd, buffer, BUFFER_SIZE)) > 0)
+// 	{
+// 		i = j = k = a = b = 0;
+// 		buffer[bytes] = '\0';
+// 		if (!me)
+// 		{
+// 			me = malloc(ft_strlen(buffer) * sizeof(char));
+// 			while (buffer[i])
+// 			{
+// 				me[i] = buffer[i];
+// 				i++;
+// 			}
+// 		}
+// 		else
+// 		{
+// 			you = malloc((ft_strlen(me) + 1) * sizeof(char));
+// 			while (me[i])
+// 			{
+// 				you[i] = me[i];
+// 				i++;
+// 			}
+// 			free(me);
+// 			me = malloc((ft_strlen(you) + ft_strlen(buffer) + 1)
+// 					* sizeof(char));
+// 			while (you[j])
+// 			{
+// 				me[j] = you[j];
+// 				j++;
+// 			}
+// 			free(you);
+// 			while (buffer[j])
+// 				me[j++] = buffer[k++];
+// 			me[j] = '\0';
+// 		}
+// 		while (strchr(me, '\n'))
+// 		{
+// 			while (me[a] != '\n')
+// 				a++;
+// 			line = malloc((a + 1) * sizeof(char));
+// 			a = 0;
+// 			while (me[a] != '\n')
+// 			{
+// 				line[a] = me[a];
+// 				a++;
+// 			}
+// 			line[a] = '\0';
+// 			b = a;
+// 			while (me[a] != '\0')
+// 				a++;
+// 			you = malloc(a * sizeof(char));
+// 			a = 0;
+// 			while (me[b] != '\0')
+// 				you[a++] = me[b++];
+// 			you[a] = '\0';
+// 			free(me);
+// 			b = 0;
+// 			me = malloc(a * sizeof(char));
+// 			while (you[b] != '\0')
+// 			{
+// 				me[b] = you[b];
+// 				b++;
+// 			}
+// 			free(you);
+// 			return (line);
+// 		}
+// 	}
+// 	if (bytes == 0 && me)
+// 	{
+// 		while (me[a] != '\0')
+// 			a++;
+// 		line = malloc((a + 1) * sizeof(char));
+// 		a = 0;
+// 		while (me[a] != '\0')
+// 		{
+// 			line[a] = me[a];
+// 			a++;
+// 		}
+// 		line[a] = '\0';
+// 		b = a;
+// 		while (me[a] != '\0')
+// 			a++;
+// 		you = malloc(a * sizeof(char));
+// 		a = 0;
+// 		while (me[b] != '\0')
+// 			you[a++] = me[b++];
+// 		you[a] = '\0';
+// 		free(me);
+// 		b = 0;
+// 		me = malloc(a * sizeof(char));
+// 		while (you[b] != '\0')
+// 		{
+// 			me[b] = you[b];
+// 			b++;
+// 		}
+// 		free(you);
+// 		return (line);
+// 	}
+// 	return (NULL);
 // }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -8,14 +8,18 @@
 # include <string.h>
 # include <unistd.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10000
+# endif
+
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
 char	*opp_strchr(char *str, int n);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strchr(const char *str, int c);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+char	*ft_strdup(const char *str);
+char	*rm_str(char *me);
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10000
-# endif
 
 #endif
